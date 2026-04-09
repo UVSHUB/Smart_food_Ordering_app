f4d396e345305092c25beae8a19305453cd56e3f
IT24101033 <it24101033@my.sliit.lk>
2026-04-09 10:14:39 +0530

    fix: App.js ReferenceError crash by importing Platform from react-native

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
